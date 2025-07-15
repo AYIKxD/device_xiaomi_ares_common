1ae59ef2389c607a3a438c0fadf6e0d4af8b7dda
rio004 <riyon336@gmail.com>
2025-07-15 20:11:35 +0100

    ares: Import project init script
    Change-Id: Ic56ac81da7a6052829196fe276ce679febc66d19
    Signed-off-by: rio004 <riyon336@gmail.com>

--- device.mk
+++ device.mk
@@ -7,6 +7,10 @@
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.project.rc
+
 # Inherit from mt6893-common
 $(call inherit-product, device/xiaomi/mt6893-common/mt6893.mk)
 
